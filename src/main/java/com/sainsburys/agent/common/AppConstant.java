47b6f709afc519e3d9fb28ffa71568457783c84c
Hitesh Javeri <hitesh.javeri@sainsburys.co.uk>
2026-05-06 11:43:15 +0100

    added constants

--- src/main/java/com/sainsburys/agent/common/AppConstant.java
+++ src/main/java/com/sainsburys/agent/common/AppConstant.java
@@ -2,8 +2,29 @@ package com.sainsburys.agent.common;
 
 public class AppConstant {
 
+    public static final String QUERY_PROMOTIONS = "query_promotions";
+    public static final String GET_PROMOTION_BY_ID = "get_promotion_by_id";
+    public static final String COUNT_PROMOTIONS = "count_promotions";
+    public static final String QUERY_PRICES = "query_prices";
+    public static final String GET_PRICE_BY_ID = "get_price_by_id";
+    public static final String GET_CURRENT_PRICE = "get_current_price";
+    public static final String GET_OFFER_TYPE_DETAIL = "get_coffer_type_detail";
+    public static final String QUERY_OFFER_TYPES = "query_offer_types";
+    public static final String GET_ALL_OFFER_TYPES = "get_all_offer_types";
+
     public static final String MESSAGE_ROLE_USER = "user";
     public static final String MESSAGE_ROLE_ASSISTANT = "assistant";
+
+    public static final String PROMO_TYPE_DESC = "promoTypeDesc";
+    public static final String REWARD_MECHANIC_TYPE = "rewardMechanicType";
+    public static final String ERROR_MESSAGE = "error";
+    public static final String PARAM_PRODUCT_CODE = "productCode";
+    public static final String PARAM_PRICE_LEVEL = "priceLevel";
+    public static final String PARAM_PRICE_CODE = "priceCode";
+    public static final String PARAM_OFFER_TYPE = "offerType";
+    public static final String PARAM_PROMOTION_NAME = "promotionName";
+    public static final String PARAM_PUBLISHED = "published";
+    public static final String PROP_PRICE_START_DATE = "priceStartDate.$date";
 
     private AppConstant() {
         /* This utility class should not be instantiated */
